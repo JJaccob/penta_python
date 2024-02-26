af1c866d741ce0f1f97b33bc760092a52d5cea67
JJaccob <107562801+JJaccob@users.noreply.github.com>
2024-02-26 19:13:19 +0300

    Add files via upload
    The file for the first assignment on introduction to Python.

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name=str(input("What is your name? "))
+age=int(input("What is your age? "))
+location=str(input("Where do you live? "))
+print("Hello", name, "you are", age, "years old", "and live in", location,)
